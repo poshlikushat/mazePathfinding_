31200bfba348807e2ec592f2ff4e1b2eca33586a
poshlikushat <161361012+poshlikushat@users.noreply.github.com>
2025-04-24 01:21:18 +0700

    edited CMake

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,12 @@ int main(int argc, char** argv) {
     std::vector<int> path = maze.getPath();
 
     if (path.empty()) {
-      std::cout << "Пути нет\n";
+      std::cout << "There is no way\n";
       return 0;
     }
 
-    std::cout << "Кратчайший путь: " << path.size() - 1 << " шаг(ов)\n";
-    std::cout << "Маршрут (row, col):\n";
+    std::cout << "The shortest path: " << path.size() - 1 << " step(s)\n";
+    std::cout << "Route (row, col):\n";
 
     for (int idx : path) {
       int r = idx / rep.cols;
